08e065dc8f56d5ab2a696a27af077697d96bcf20
Prajjwal Basnet <prajjwalbasnet@192-168-1-113.tpgi.com.au>
2023-01-30 15:14:05 +1100

    Second

--- test.py
+++ test.py
@@ -11,11 +11,11 @@ def next_turn(row,column):
             buttons[row][column]['text'] = player
 
             if check_winner()is False:
-                player = player[1]
-                label.config(text=(player[1]+" turn"))
+                player = players[1]
+                label.config(text=(player+" turn"))
 
             elif check_winner()is True:
-                label.config(text=player[0]+" wins")
+                label.config(text=player+" wins")
 
             elif check_winner() == "Tie":
                 label.config(text="Its a tie")
@@ -25,36 +25,62 @@ def next_turn(row,column):
             buttons[row][column]['text'] = player
 
             if check_winner() is False:
-                player = player[0]
-                label.config(text=(player[0] + " turn"))
+                player = players[0]
+                label.config(text=(player + " turn"))
 
             elif check_winner() is True:
-                label.config(text=player[1] + " wins")
+                label.config(text=player + " wins")
 
             elif check_winner() == "Tie":
                 label.config(text="Its a tie")
 
 
 def empty_spaces():
-    pass
+    spaces = 9
+
+    for row in range(3):
+        for column in range(3):
+            if buttons[row][column]['text']!="":
+                spaces-=1
+    if spaces == 0:
+        return False
+    else:
+        return True
 
 def check_winner():
 
     for row in range(3):
         if buttons[row][0]['text'] == buttons[row][1]['text']== buttons[row][2]['text'] != "":
+            buttons[row][0].config(bg="green")
+            buttons[row][1].config(bg="green")
+            buttons[row][2].config(bg="green")
             return True
+
 
     for column in range(3):
         if buttons[0][column]['text'] == buttons[1][column]['text'] == buttons[2][column]['text'] != "":
+            buttons[0][column].config(bg="green")
+            buttons[1][column].config(bg="green")
+            buttons[2][column].config(bg="green")
             return True
 
     if buttons[0][0]['text'] == buttons[1][1]['text'] == buttons[2][2]['text'] != "":
-            return True
+        buttons[0][0].config(bg="green")
+        buttons[1][1].config(bg="green")
+        buttons[2][2].config(bg="green")
+        return True
 
     elif buttons[0][2]['text'] == buttons[1][1]['text'] == buttons[2][0]['text'] != "":
+        buttons[0][2].config(fill="green")
+        buttons[1][1].config(fill="green")
+        buttons[2][0].config(fill="green")
         return True
 
     elif empty_spaces() is False:
+
+        for row in range(3):
+            for column in range(3):
+                buttons[row][column].config(bg="Yellow")
         return "Tie"
     else:
         return False
@@ -62,7 +88,16 @@ def check_winner():
 
 
 def new_game():
-    pass
+    global players
+
+    player = random.choice(players)
+
+    label.config(text=player+" turn")
+
+    for row in range(3):
+        for column in range(3):
+            buttons[row][column].config(text="",bg="#F0F0F0")
+
 
 
 window = Tk()
@@ -76,7 +111,7 @@ buttons = [[0,0,0],
 label = Label(text=player+" turn", font=('consolas',40))
 label.pack(side=TOP)
 
-reset_button = Button(text = "restart", font=('consolas',20), command=new_game())
+reset_button = Button(text = "restart", font=('consolas',20), command=new_game)
 reset_button.pack(side="top")
 
 frame = Frame(window)
@@ -84,8 +119,7 @@ frame.pack()
 
 for row in range(3):
     for column in range(3):
-        buttons[row][column] = Button(frame, text="", font=('consolas',40),width=3,height=3,
-                                      command = lambda row=row,column=column: next_turn())
+        buttons[row][column] = Button(frame, text="", font=('consolas',40),width=3,height=3, command = lambda row=row,column=column: next_turn(row,column))
         buttons[row][column].grid(row=row,column=column)
 
 
